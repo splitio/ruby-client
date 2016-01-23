28d315fad2c7da3f7aead8a6b7cc3e10f6742d5d
Boris Ortiz <bortiz@MiniMac2510s-Mac-mini.local>
2016-01-22 19:50:23 -0600

    Added refactors for metrics and impressions modules

--- lib/splitclient-engine/impressions/impressions.rb
+++ lib/splitclient-engine/impressions/impressions.rb
@@ -1,6 +1,40 @@
 module SplitIoClient
 
   class Impressions < NoMethodError
+
+    attr_reader :queue
+    attr_reader :max_number_of_keys
+
+    def initialize(max)
+      @queue = {}
+      @max_number_of_keys =  max
+    end
+
+    def log(id, feature, treatment, time)
+      impressions =  @queue.find{|i| i[:feature] == feature}
+      if impressions.nil?
+        impressions << KeyImpressions.new(id, treatment, time)
+        @queue.merge!({feature:feature, impressions: impressions})
+      elsif impressions.size >= @max_number_of_keys
+        impressions << KeyImpressions.new(id, treatment, time)
+        @queue.merge!({feature:feature, impressions: impressions})
+      else
+        impressions << KeyImpressions.new(id, treatment, time)
+      end
+      return
+    end
+  end
+
+  class KeyImpressions
+    attr_accessor :key
+    attr_accessor :treatment
+    attr_accessor :time
+
+    def initialize(key, treatment, time)
+      @key =  key
+      @treatment = treatment
+      @time = time
+    end
   end
 
 end
--- lib/splitclient-engine/metrics/metrics.rb
+++ lib/splitclient-engine/metrics/metrics.rb
@@ -1,6 +1,83 @@
 module SplitIoClient
 
   class Metrics < NoMethodError
+
+    @counter
+    @delta
+    @latency_hash
+    @count_hash
+    @gauge_hash
+
+    def initialize
+      @latency_hash = {}
+      @count_hash = {}
+      @gauge_hash = {}
+    end
+
+    def count(counter, delta)
+      return if delta <= 0
+
+      return if (counter.nil? || counter.strip.empty?)
+
+      if_present = @count_hash.find{|c| c[:counter] == counter}
+      if if_present.nil?
+        if_present = SumAndCount.new
+        new_count_hash = {counter:counter, sum_and_count:if_present}
+        @count_hash.merge!(new_count_hash)
+      end
+
+      if_present.add_delta(delta)
+    end
+
+    def time(operation, time)
+      return
+    end
+
+    def gauge(gauge, value)
+      return
+    end
+
+  end
+
+  class SumAndCount
+    attr_reader :count
+    attr_reader :sum
+
+    def initialize
+      @count = 0
+      @sum = 0
+    end
+
+    def add_delta(delta)
+      @count++
+      @sum += delta
+    end
+
+    def clear
+      @count = 0
+      @sum = 0
+    end
+  end
+
+  class ValueAndCount
+    attr_reader :count
+    attr_reader :value
+
+    def initialize
+      @count = 0
+      @value = 0
+    end
+
+    def setValue(value)
+      @count++
+      @value = value
+    end
+
+    def clear
+      @count = 0
+      @value = 0
+    end
+
   end
 
 end
