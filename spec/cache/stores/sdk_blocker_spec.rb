0627de52d0f0ceb88874a090ef2a0cf9beffef34
Maxim Filippov <colixer@gmail.com>
2016-09-30 21:59:21 +0300

    Fix spec

--- spec/cache/stores/sdk_blocker_spec.rb
+++ spec/cache/stores/sdk_blocker_spec.rb
@@ -5,13 +5,17 @@ describe SplitIoClient::Cache::Stores::SDKBlocker do
   let(:sdk_blocker) { described_class.new(config) }
 
   it 'raises SDKBlockerTimeoutException' do
-    allow(sdk_blocker).to receive(:sdk_ready?).and_return(false)
+    sdk_blocker.instance_variable_set(:@splits_thread, Thread.new { sleep })
+    sdk_blocker.instance_variable_set(:@segments_thread, Thread.new { sleep })
 
     expect { sdk_blocker.when_ready }.to raise_exception(SplitIoClient::SDKBlockerTimeoutExpiredException)
   end
 
   it 'does not raise SDKBlockerTimeoutException when ready' do
-    allow(sdk_blocker).to receive(:sdk_ready?).and_return(true)
+    sdk_blocker.instance_variable_set(:@splits_ready, true)
+    sdk_blocker.instance_variable_set(:@segments_ready, true)
+    sdk_blocker.instance_variable_set(:@splits_thread, Thread.new {})
+    sdk_blocker.instance_variable_set(:@segments_thread, Thread.new {})
 
     expect { sdk_blocker.when_ready { 1 } }.not_to raise_exception
   end
