d2277c4c2a1e62126013f4bca63d700a34bbde75
Patricio Echague <patricioe@gmail.com>
2016-10-18 15:32:52 -0700

    make test work

--- spec/splitclient-rb_corner_cases_spec.rb
+++ spec/splitclient-rb_corner_cases_spec.rb
@@ -4,6 +4,7 @@ describe SplitIoClient do
   subject { SplitIoClient::SplitFactory.new('', { logger: Logger.new('/dev/null') }).client }
 
   let(:splits_json) { File.read(File.expand_path(File.join(File.dirname(__FILE__), 'test_data/splits/splits.json'))) }
+  let(:segments_json) { File.read(File.expand_path(File.join(File.dirname(__FILE__), 'test_data/segments/segmentNoOneUses.json'))) }
 
   let(:user) { 'fake_user_id_1' }
   let(:feature) { 'test_1_ruby' }
@@ -14,10 +15,13 @@ describe SplitIoClient do
   before do
     stub_request(:get, 'https://sdk.split.io/api/splitChanges?since=-1')
       .to_return(status: 200, body: splits_json)
+
+    stub_request(:get, "https://sdk.split.io/api/segmentChanges/demo?since=-1")
+      .to_return(status: 200, body: [])
   end
 
-  it 'validates the feature is CONTROL for id when segment used does not exist' do
-    expect(subject.get_treatment(user, feature)).to eq SplitIoClient::Treatments::CONTROL
+  it 'validates the feature is "default" for id when segment used does not exist' do
+    expect(subject.get_treatment(user, feature)).to eq "default"
   end
 
 end
