b6f7299484d025a79eb0b14bb86ec0ecf6a592bb
Maxim Filippov <colixer@gmail.com>
2016-11-16 01:31:29 +0400

    Limit queue size (#82)
    * Limit queue size
    
    * Refactor queue adapter
    
    * Decrement on each iteration

--- lib/cache/adapters/memory_adapters/queue_adapter.rb
+++ lib/cache/adapters/memory_adapters/queue_adapter.rb
@@ -4,24 +4,32 @@ module SplitIoClient
       module MemoryAdapters
         # Memory adapter implementation, which stores everything inside queue
         class QueueAdapter
-          def initialize(size)
-            @size = size
+          def initialize(max_size)
+            @max_size = max_size
             @queue = Queue.new
+            @current_size = Concurrent::AtomicFixnum.new(0)
           end
 
           # Adds data to queue in non-blocking mode
           def add_to_queue(data)
+            fail ThreadError if @current_size.value >= @max_size
+
             @queue.push(data)
+
+            @current_size.increment
           end
 
           # Get all items from the queue
           def clear
             items = []
 
-            loop { items << @queue.pop(true) }
+            loop do
+              items << @queue.pop(true)
+
+              @current_size.decrement
+            end
 
           rescue ThreadError
-            # Last queue item reached
             items
           end
         end
---
+++ spec/cache/adapters/memory/queue_adapter_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe SplitIoClient::Cache::Adapters::MemoryAdapters::QueueAdapter do
+  let(:queue_size) { 3 }
+  let(:adapter) { described_class.new(queue_size) }
+
+  before do
+    queue_size.times { adapter.add_to_queue('foo') }
+  end
+
+  it 'throws exception if queue size is reached' do
+    expect { adapter.add_to_queue('foo') }.to raise_error(ThreadError)
+  end
+
+  it 'sets correct current_size after clear' do
+    adapter.clear
+
+    expect(adapter.instance_variable_get(:@current_size).value).to eq(0)
+  end
+
+  it 'adds impressions' do
+    expect(adapter.instance_variable_get(:@current_size).value).to eq(queue_size)
+  end
+end
