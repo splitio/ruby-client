7be14ccd98503f3fa82b4a1720f67dde838cdfab
Patricio Echague <patricioe@gmail.com>
2016-11-18 13:28:12 -0800

    support .splits as default too

--- lib/splitclient-rb/split_factory_builder.rb
+++ lib/splitclient-rb/split_factory_builder.rb
@@ -3,7 +3,8 @@ module SplitIoClient
     def self.build(api_key, config = {})
       case api_key
       when 'localhost'
-        splits_file = config[:path]
+        splits_file = config[:path]? config[:path] : File.join(Dir.home, '.split')
+        puts("Filename #{splits_file}")
 
         LocalhostSplitFactory.new(splits_file)
       else
