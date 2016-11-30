df8c75d88f515ba4b2fe100ee90d85f89e49baa7
Maxim Filippov <colixer@gmail.com>
2016-11-30 23:29:06 +0300

    Add SDK blocker specs

--- spec/cache/stores/sdk_blocker_spec.rb
+++ spec/cache/stores/sdk_blocker_spec.rb
@@ -1,22 +1,55 @@
 require 'spec_helper'
 
 describe SplitIoClient::Cache::Stores::SDKBlocker do
-  let(:config) { SplitIoClient::SplitConfig.new(block_until_ready: 1) }
-  let(:sdk_blocker) { described_class.new(config) }
+  RSpec.shared_examples 'sdk_blocker specs' do |cache_adapter|
+    let(:config) { SplitIoClient::SplitConfig.new(block_until_ready: 0.1) }
+    let(:splits_repository) { SplitIoClient::Cache::Repositories::SplitsRepository.new(cache_adapter) }
+    let(:segments_repository) { SplitIoClient::Cache::Repositories::SegmentsRepository.new(cache_adapter) }
+    let(:sdk_blocker) { described_class.new(config, splits_repository, segments_repository) }
 
-  xit 'raises SDKBlockerTimeoutException' do
-    sdk_blocker.instance_variable_set(:@splits_thread, Thread.new { sleep })
-    sdk_blocker.instance_variable_set(:@segments_thread, Thread.new { sleep })
+    before :each do
+      redis = Redis.new
+      redis.flushall
+    end
 
-    expect { sdk_blocker.when_ready { 1 } }.to raise_exception(SplitIoClient::SDKBlockerTimeoutExpiredException)
+    it 'is not ready after initialization' do
+      sdk_blocker
+
+      expect(splits_repository.ready?).to be(false)
+    end
+
+    it 'is ready when both splits and segments are ready' do
+      sdk_blocker.splits_ready!
+      sdk_blocker.segments_ready!
+
+      expect(sdk_blocker.ready?).to be true
+    end
+
+    it 'throws exception if not ready' do
+      expect { sdk_blocker.block }.to raise_error(SplitIoClient::SDKBlockerTimeoutExpiredException)
+    end
+
+    it 'runs threads when ready' do
+      sdk_blocker.splits_thread = Thread.new { Thread.stop }
+      sdk_blocker.segments_thread = Thread.new { Thread.stop }
+
+      sleep 0.1
+
+      expect(sdk_blocker.instance_variable_get(:@splits_thread).status).to eq('sleep')
+      expect(sdk_blocker.instance_variable_get(:@segments_thread).status).to eq('sleep')
+
+      sdk_blocker.splits_ready!
+      sdk_blocker.segments_ready!
+
+      sdk_blocker.block
+
+      sleep 0.1
+
+      expect(sdk_blocker.instance_variable_get(:@splits_thread).status).to eq(false)
+      expect(sdk_blocker.instance_variable_get(:@segments_thread).status).to eq(false)
+    end
   end
 
-  xit 'does not raise SDKBlockerTimeoutException when ready' do
-    sdk_blocker.instance_variable_set(:@splits_ready, true)
-    sdk_blocker.instance_variable_set(:@segments_ready, true)
-    sdk_blocker.instance_variable_set(:@splits_thread, Thread.new {})
-    sdk_blocker.instance_variable_set(:@segments_thread, Thread.new {})
-
-    expect { sdk_blocker.when_ready { 1 } }.not_to raise_exception
-  end
+  include_examples 'sdk_blocker specs', SplitIoClient::Cache::Adapters::MemoryAdapter.new(SplitIoClient::Cache::Adapters::MemoryAdapters::SizedQueueAdapter.new(3))
+  include_examples 'sdk_blocker specs', SplitIoClient::Cache::Adapters::RedisAdapter.new(SplitIoClient::SplitConfig.new.redis_url)
 end
