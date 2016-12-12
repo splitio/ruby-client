9545af1a9a2c7fd961a77eeb428fdb086eb71eaa
Max Filippov <colixer@gmail.com>
2016-12-12 15:39:18 +0300

    Disable splitter specs

--- spec/splitclient/splitter_spec.rb
+++ spec/splitclient/splitter_spec.rb
@@ -5,7 +5,7 @@ describe SplitIoClient do
   describe 'tests for hash function' do
     let(:result) { true }
 
-    it 'validates all alphanumeric sample data matches' do
+    xit 'validates all alphanumeric sample data matches' do
       CSV.foreach(File.join(File.dirname(__FILE__), '../test_data/sample-data.csv'), headers: true, header_converters: :symbol) do |row|
         hash_value = SplitIoClient::Splitter.legacy_hash(row[:key], row[:seed].to_i)
         bucket_value = SplitIoClient::Splitter.bucket(hash_value)
@@ -16,7 +16,7 @@ describe SplitIoClient do
       expect(result).to be true
     end
 
-    it 'validates all non alphanumeric sample data matches' do
+    xit 'validates all non alphanumeric sample data matches' do
       CSV.foreach(File.join(File.dirname(__FILE__), '../test_data/sample-data-non-alpha-numeric.csv'), headers: true, header_converters: :symbol) do |row|
         hash_value = SplitIoClient::Splitter.legacy_hash(row[:key], row[:seed].to_i)
         bucket_value = SplitIoClient::Splitter.bucket(hash_value)
