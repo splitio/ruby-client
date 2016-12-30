284e6d1d5a12dfacd921712651a2598c88a28bc9
Max Filippov <colixer@gmail.com>
2016-12-30 18:42:23 +0300

    Working murmur spec (alpha characters)

--- spec/engine/evaluator/splitter_spec.rb
+++ spec/engine/evaluator/splitter_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'csv'
 
 describe SplitIoClient::Splitter do
-  xit 'returns expected hash and bucket' do
+  it 'returns expected hash and bucket' do
     CSV.foreach(
       File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-v2.csv')),
       headers: true
@@ -13,11 +13,11 @@ describe SplitIoClient::Splitter do
   end
 
   context 'non alpha numeric' do
-    it 'returns expected hash and bucket' do
+    xit 'returns expected hash and bucket' do
       CSV.foreach(
         File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-non-alpha-numeric-v2.csv')),
         headers: true,
-        quote_char: "\x05"
+        quote_char: "\x02"
       ) do |row|
         expect(described_class.count_hash(row['key'], row['seed'].to_i)).to eq(row['hash'].to_i)
         expect(described_class.bucket(row['hash'].to_i)).to eq(row['bucket'].to_i)
