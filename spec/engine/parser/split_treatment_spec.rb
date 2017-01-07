aab321304a0648cb4f54db20c93f73df085dbe82
Patricio Echague <patricioe@gmail.com>
2017-01-07 15:07:25 -0800

    add change_number to broken tests

--- spec/engine/parser/split_treatment_spec.rb
+++ spec/engine/parser/split_treatment_spec.rb
@@ -12,10 +12,10 @@ describe Engine::Parser::SplitTreatment do
   let(:archived_split) { { status: 'ARCHIVED' } }
 
   it 'returns killed treatment' do
-    expect(split_treatment.call('foo', killed_split)).to eq({ label: 'killed', treatment: 'default' })
+    expect(split_treatment.call('foo', killed_split)).to eq({ label: 'killed', treatment: 'default', change_number: nil})
   end
 
   it 'returns archived treatment' do
-    expect(split_treatment.call('foo', archived_split)).to eq({ label: 'archived', treatment: Treatments::CONTROL })
+    expect(split_treatment.call('foo', archived_split)).to eq({ label: 'archived', treatment: Treatments::CONTROL, change_number: nil })
   end
 end
