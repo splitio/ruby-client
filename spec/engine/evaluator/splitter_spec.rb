3579eade4b5f960c42c2777cdf91dafec9a9afac
Max Filippov <colixer@gmail.com>
2017-01-17 13:39:21 +0300

    Fix specs for all non-alpha entries

--- spec/engine/evaluator/splitter_spec.rb
+++ spec/engine/evaluator/splitter_spec.rb
@@ -1,27 +1,17 @@
 require 'spec_helper'
-require 'csv'
 
 describe SplitIoClient::Splitter do
-  it 'returns expected hash and bucket' do
-    CSV.foreach(
-      File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-v2.csv')),
-      headers: true
-    ) do |row|
-      expect(described_class.count_hash(row['key'], row['seed'].to_i)).to eq(row['hash'].to_i)
-      expect(described_class.bucket(row['hash'].to_i)).to eq(row['bucket'].to_i)
-    end
-  end
-
-  context 'non alpha numeric' do
+  RSpec.shared_examples 'murmur3 sample data' do |file|
     it 'returns expected hash and bucket' do
-      CSV.foreach(
-        File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-non-alpha-numeric-v2.csv')),
-        headers: true,
-        quote_char: "\x02"
-      ) do |row|
-        expect(described_class.count_hash(row['key'], row['seed'].to_i)).to eq(row['hash'].to_i)
-        expect(described_class.bucket(row['hash'].to_i)).to eq(row['bucket'].to_i)
+      File.foreach(file) do |row|
+        seed, key, hash, bucket = row.split(',')
+
+        expect(described_class.count_hash(key, seed.to_i)).to eq(hash.to_i)
+        expect(described_class.bucket(hash.to_i)).to eq(bucket.to_i)
       end
     end
   end
+
+  include_examples 'murmur3 sample data', File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-v2.csv'))
+  include_examples 'murmur3 sample data', File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/hash/murmur3-sample-data-non-alpha-numeric-v2.csv'))
 end
