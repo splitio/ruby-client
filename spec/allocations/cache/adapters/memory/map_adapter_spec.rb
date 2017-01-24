be97c18ccda00a50533559a76e2ffe70aa54ec12
Max Filippov <colixer@gmail.com>
2017-01-24 12:30:20 +0300

    WIP

--- spec/allocations/cache/adapters/memory/map_adapter_spec.rb
+++ spec/allocations/cache/adapters/memory/map_adapter_spec.rb
@@ -4,12 +4,13 @@ describe SplitIoClient::Cache::Adapters::MemoryAdapters::MapAdapter do
   let(:mri_allocations) { 2 }
   let(:adapter) { described_class.new }
   let(:key) { 'foo'.freeze }
+  let(:key2) { 'bar'.freeze }
 
   before(:each) do
     adapter
   end
 
-  it 'initializes MaxAdapter' do
+  it 'initializes MapAdapter' do
     expect { adapter }.to allocate_max(mri_allocations + 1).objects
   end
 
@@ -39,5 +40,17 @@ describe SplitIoClient::Cache::Adapters::MemoryAdapters::MapAdapter do
     adapter.add_to_map(key, key, key)
 
     expect { adapter.in_map?(key, key) }.to allocate_max(mri_allocations).objects
+  end
+
+  it 'sets string' do
+    expect { adapter.set_string(key, key) }.to allocate_max(mri_allocations).objects
+  end
+
+  it 'unions sets' do
+    adapter.initialize_set(key)
+    adapter.add_to_set(key, [key, key])
+    adapter.add_to_set(key2, [key2, key2])
+
+    expect { adapter.union_sets([key, key2]) }.to allocate_max(mri_allocations + 9).objects
   end
 end
---
+++ spec/allocations/cache/repositories/impressions/memory_repository_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+include SplitIoClient::Cache::Adapters
+
+describe SplitIoClient::Cache::Repositories::Impressions::MemoryRepository do
+  let(:config) { SplitIoClient::SplitConfig.new(impressions_queue_size: 5) }
+  let(:adapter) { MemoryAdapter.new(MemoryAdapters::SizedQueueAdapter.new(3)) }
+  let(:repository) { described_class.new(adapter, config) }
+  let(:key) { 'foo'.freeze }
+  let(:data) { { foo: 'bar' }.freeze }
+
+  it 'adds impression' do
+    repository
+    key
+    data
+
+    expect { repository.add(key, data) }.to allocate_max(1).objects
+  end
+end
