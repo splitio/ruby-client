cd066df4385c565e88f5d2db36e45e12859e447f
Maxim Filippov <colixer@gmail.com>
2017-06-06 18:21:42 +0300

    Remove useless method

--- lib/cache/routers/impression_router.rb
+++ lib/cache/routers/impression_router.rb
@@ -44,11 +44,5 @@ module SplitIoClient
         end
       end
     end
-
-    def random_interval(interval)
-      random_factor = Random.new.rand(50..100) / 100.0
-
-      interval * random_factor
-    end
   end
 end
