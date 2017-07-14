b06686004eeaa564c946591a2a8f57d319df1250
Maxim Filippov <colixer@gmail.com>
2017-07-14 21:28:21 +0300

    Fix tests

--- spec/engine/matchers/equal_to_boolean_matcher_spec.rb
+++ spec/engine/matchers/equal_to_boolean_matcher_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe SplitIoClient::EqualToBooleanMatcher do
   it 'matches' do
-    expect(described_class.new('value', true).match?(nil, value: true)).to eq(true)
-    expect(described_class.new('value', true).match?(nil, value: 'true')).to eq(true)
-    expect(described_class.new('value', true).match?(nil, value: 'tRue')).to eq(true)
-    expect(described_class.new('value', false).match?(nil, value: false)).to eq(true)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: true)).to eq(true)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: 'true')).to eq(true)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: 'tRue')).to eq(true)
+    expect(described_class.new('value', false).match?(nil, nil, nil, value: false)).to eq(true)
   end
 
   it 'does not match' do
-    expect(described_class.new('value', true).match?(nil, value: false)).to eq(false)
-    expect(described_class.new('value', true).match?(nil, value: 'false')).to eq(false)
-    expect(described_class.new('value', true).match?(nil, value: 'whatever but true')).to eq(false)
-    expect(described_class.new('value', false).match?(nil, value: true)).to eq(false)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: false)).to eq(false)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: 'false')).to eq(false)
+    expect(described_class.new('value', true).match?(nil, nil, nil, value: 'whatever but true')).to eq(false)
+    expect(described_class.new('value', false).match?(nil, nil, nil, value: true)).to eq(false)
   end
 end
--- spec/engine/matchers/matches_string_matcher_spec.rb
+++ spec/engine/matchers/matches_string_matcher_spec.rb
@@ -4,13 +4,13 @@ describe SplitIoClient::MatchesStringMatcher do
   let(:regexp_file) { File.read(File.expand_path(File.join(File.dirname(__FILE__), '../../test_data/regexp/data.txt'))) }
 
   it 'matches' do
-    expect(described_class.new('value', %r{fo.}).match?(nil, value: 'foo')).to eq(true)
-    expect(described_class.new('value', 'foo').match?(nil, value: 'foo')).to eq(true)
+    expect(described_class.new('value', %r{fo.}).match?(nil, nil, nil, value: 'foo')).to eq(true)
+    expect(described_class.new('value', 'foo').match?(nil, nil, nil, value: 'foo')).to eq(true)
   end
 
   it 'does not match' do
-    expect(described_class.new('value', %r{fo.}).match?(nil, value: 'bar')).to eq(false)
-    expect(described_class.new('value', 'foo').match?(nil, value: 'bar')).to eq(false)
+    expect(described_class.new('value', %r{fo.}).match?(nil, nil, nil, value: 'bar')).to eq(false)
+    expect(described_class.new('value', 'foo').match?(nil, nil, nil, value: 'bar')).to eq(false)
   end
 
   it 'matches sample regexps from file' do
@@ -18,7 +18,7 @@ describe SplitIoClient::MatchesStringMatcher do
       regexp_str, test_str, result_str = str.split('#')
 
       expect(
-        described_class.new('key', Regexp.new(regexp_str)).match?(nil, key: test_str)
+        described_class.new('key', Regexp.new(regexp_str)).match?(nil, nil, nil, key: test_str)
       ).to eq(result_str == 'true')
     end
   end
