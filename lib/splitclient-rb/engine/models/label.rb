9c62491fee389b7c4b76958c0ddce4c6d030d1ae
Andrew Greene <andrew.greene@split.io>
2017-12-13 16:58:40 -0800

    [generated] update SDK labels

--- lib/splitclient-rb/engine/models/label.rb
+++ lib/splitclient-rb/engine/models/label.rb
@@ -1,6 +1,6 @@
 class SplitIoClient::Engine::Models::Label
   ARCHIVED = 'archived'.freeze
-  NO_RULE_MATCHED = 'no rule matched'.freeze
+  NO_RULE_MATCHED = 'default rule'.freeze
   EXCEPTION = 'exception'.freeze
   KILLED = 'killed'.freeze
   NOT_IN_SPLIT = 'not in split'.freeze
