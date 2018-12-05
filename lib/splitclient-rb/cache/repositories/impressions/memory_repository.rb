66ea641bf1d5b37e8b475f2d40adca92c03a7400
ignaciosplit <41024906+ignaciosplit@users.noreply.github.com>
2018-12-05 13:35:43 -0300

    Impressions revamp (#225)

--- lib/splitclient-rb/cache/repositories/impressions/memory_repository.rb
+++ lib/splitclient-rb/cache/repositories/impressions/memory_repository.rb
@@ -1,42 +1,44 @@
+# frozen_string_literal: true
+
 module SplitIoClient
   module Cache
     module Repositories
       module Impressions
-        class MemoryRepository
-
+        class MemoryRepository < ImpressionsRepository
           def initialize(adapter)
             @adapter = adapter
           end
 
           # Store impression data in the selected adapter
-          def add(split_name, data)
-            @adapter.add_to_queue(feature: split_name, impressions: data)
+          def add(matching_key, bucketing_key, split_name, treatment, time)
+            @adapter.add_to_queue(
+              m: metadata,
+              i: impression_data(
+                matching_key,
+                bucketing_key,
+                split_name,
+                treatment,
+                time
+              )
+            )
           rescue ThreadError # queue is full
             if random_sampler.rand(1..1000) <= 2 # log only 0.2 % of the time
-              SplitIoClient.configuration.logger.warn("Dropping impressions. Current size is #{SplitIoClient.configuration.impressions_queue_size}. " \
-                                  "Consider increasing impressions_queue_size")
+              SplitIoClient.configuration.logger.warn("Dropping impressions. Current size is \
+                #{SplitIoClient.configuration.impressions_queue_size}. " \
+                'Consider increasing impressions_queue_size')
             end
           end
 
           def add_bulk(key, bucketing_key, treatments, time)
             treatments.each do |split_name, treatment|
-              add(
-                split_name,
-                'keyName' => key,
-                'bucketingKey' => bucketing_key,
-                'treatment' => treatment[:treatment],
-                'label' => SplitIoClient.configuration.labels_enabled ? treatment[:label] : nil,
-                'changeNumber' => treatment[:change_number],
-                'time' => time
-              )
+              add(key, bucketing_key, split_name, treatment, time)
             end
           end
 
-          def get_batch
-            return [] if SplitIoClient.configuration.impressions_bulk_size == 0
-            @adapter.get_batch(SplitIoClient.configuration.impressions_bulk_size).map do |impression|
-              impression.update(ip: SplitIoClient.configuration.machine_ip)
-            end
+          def batch
+            return [] if SplitIoClient.configuration.impressions_bulk_size.zero?
+
+            @adapter.get_batch(SplitIoClient.configuration.impressions_bulk_size)
           end
 
           private
--- lib/splitclient-rb/cache/senders/impressions_formatter.rb
+++ lib/splitclient-rb/cache/senders/impressions_formatter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SplitIoClient
   module Cache
     module Senders
@@ -7,29 +9,15 @@ module SplitIoClient
         end
 
         def call(raw_impressions)
-          impressions = raw_impressions ? raw_impressions : @impressions_repository.get_batch
-          formatted_impressions = []
+          impressions = raw_impressions || @impressions_repository.batch
           filtered_impressions = filter_impressions(impressions)
 
           return [] if impressions.empty? || filtered_impressions.empty?
 
           formatted_impressions = unique_features(filtered_impressions).each_with_object([]) do |feature, memo|
-            ip = nil
-            current_impressions =
-              filtered_impressions
-                .select { |impression| impression[:feature] == feature }
-                .map do |impression|
-                  ip = impression[:ip]
-                  {
-                    keyName: impression[:impressions]['keyName'] || impression[:impressions]['key_name'],
-                    treatment: impression[:impressions]['treatment'],
-                    time: impression[:impressions]['time'],
-                    bucketingKey: impression[:impressions]['bucketingKey'] || impression[:impressions]['bucketing_key'],
-                    label: impression[:impressions]['label'],
-                    changeNumber: impression[:impressions]['changeNumber'] || impression[:impressions]['change_number'],
-                  }
-                end
-
+            feature_impressions = feature_impressions(filtered_impressions, feature)
+            ip = feature_impressions.first[:m][:i]
+            current_impressions = current_impressions(feature_impressions)
             memo << {
               testName: feature.to_sym,
               keyImpressions: current_impressions,
@@ -42,8 +30,27 @@ module SplitIoClient
 
         private
 
+        def feature_impressions(filtered_impressions, feature)
+          filtered_impressions.select do |impression|
+            impression[:i][:f] == feature
+          end
+        end
+
+        def current_impressions(feature_impressions)
+          feature_impressions.map do |impression|
+            {
+              keyName: impression[:i][:k],
+              treatment: impression[:i][:t],
+              time: impression[:i][:m],
+              bucketingKey: impression[:i][:b],
+              label: impression[:i][:r],
+              changeNumber: impression[:i][:c]
+            }
+          end
+        end
+
         def unique_features(impressions)
-          impressions.map { |impression| impression[:feature] }.uniq
+          impressions.map { |impression| impression[:i][:f] }.uniq
         end
 
         # Filter seen impressions by impression_hash
@@ -61,11 +68,11 @@ module SplitIoClient
         end
 
         def impression_hash(impression)
-          "#{impression[:feature]}:" \
-          "#{impression[:impressions]['keyName']}:" \
-          "#{impression[:impressions]['bucketingKey']}:" \
-          "#{impression[:impressions]['changeNumber']}:" \
-          "#{impression[:impressions]['treatment']}"
+          "#{impression[:i][:f]}:" \
+          "#{impression[:i][:k]}:" \
+          "#{impression[:i][:b]}:" \
+          "#{impression[:i][:c]}:" \
+          "#{impression[:i][:t]}"
         end
       end
     end
