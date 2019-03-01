142a78c3eacdc31d2ba3d7ec6078782244010afb
ezesplit <41024946+ezesplit@users.noreply.github.com>
2019-03-01 10:49:51 -0300

    Faraday net http persistent compatibility issue (#244)
    * Fix compatiblity issue with Faraday < 0.13 with net-http-persistent >= 3.0 (#238)
    
    Merging to temporary branch to wrap up work and prepare for release
    
    * WIP: CR feedback applied
    Missing: Appraisal gemfiles
    
    * WIP: Appraisal gemfiles
    TODO: check default rake task
    
    * Fix rspec run twice on default raketask

---
+++ spec/engine/api/client_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SplitIoClient::Api::Client do
+  describe '#get_api' do
+    it 'makes GET request without error' do
+      url     = 'https://example.org?hello=world'
+      api_key = 'abc-def-ghi'
+      params  = { hello: :world }
+
+      stub_request(:get, url).to_return(status: 200)
+
+      expect { described_class.new.get_api(url, api_key, params) }.not_to raise_error
+    end
+
+    it 'makes POST request without error' do
+      url     = 'https://example.org'
+      api_key = 'abc-def-ghi'
+      data    = { hello: :world }
+
+      stub_request(:post, url).to_return(status: 200)
+
+      expect { described_class.new.post_api(url, api_key, data) }.not_to raise_error
+    end
+
+    if described_class.new.send(:needs_patched_net_http_persistent_adapter?)
+      it 'uses PatchedNetHttpPersistent middleware' do
+        url     = 'https://example.org?hello=world'
+        api_key = 'abc-def-ghi'
+        params  = { hello: :world }
+
+        stub_request(:get, url).to_return(status: 200)
+
+        expect_any_instance_of(SplitIoClient::FaradayAdapter::PatchedNetHttpPersistent)
+          .to receive(:net_http_connection).and_call_original
+
+        described_class.new.get_api(url, api_key, params)
+      end
+    end
+  end
+end
