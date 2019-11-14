493631e67bdd009cf45ace3e9dbed26c37526f5b
Mauro Sanz <mauro.sanz@split.io>
2019-11-14 14:28:39 -0300

    checking message content

--- lib/splitclient-rb/split_logger.rb
+++ lib/splitclient-rb/split_logger.rb
@@ -5,11 +5,18 @@ module SplitIoClient
       end
 
       def log_if_debug(message)
-        @config.logger.debug(message) if @config.debug_enabled
+        @config.logger.debug(message) if enabled_log?(@config.debug_enabled, message)
       end
 
       def log_if_transport(message)
-        @config.logger.debug(message) if @config.transport_debug_enabled
+        @config.logger.debug(message) if enabled_log?(@config.transport_debug_enabled, message)
+      end
+
+      private
+      def enabled_log?(debug_enabled, message)
+        message = message.to_s
+
+        debug_enabled && !message.empty? && message != '{}'
       end
   end
 end
