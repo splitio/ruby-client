73f2538b89ce766d36014c666f1f5e77f4e5f483
Mauro Sanz <mauro.sanz@split.io>
2020-03-11 18:24:02 -0300

    auth service implementation

--- lib/splitclient-rb/engine/auth_api_client.rb
+++ lib/splitclient-rb/engine/auth_api_client.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
+require 'jwt'
+
 module SplitIoClient
   module Engine
     class AuthApiClient
+      def initialize(config)
+        @config = config
+        @api_client = SplitIoClient::Api::Client.new(@config)
+      end
+
       def authenticate(api_key)
-        # TODO: implement this method
-        { push_enabled: true, api_key: api_key, token: 'token-fake', channels: 'channel-fake', status_code: 200 }
+        response = @api_client.get_api(@config.auth_service_url, api_key)
+
+        if response.success?
+          body_json = JSON.parse(response.body, symbolize_names: true)
+          push_enabled = body_json[:pushEnabled]
+          token = body_json[:token]
+
+          return {
+            push_enabled: push_enabled,
+            token: token,
+            channels: (channels(token) if push_enabled),
+            retry: false
+          }
+        elsif response.status >= 400 && response.status < 500
+          return { push_enabled: false, retry: false }
+        end
+
+        { push_enabled: false, retry: true }
+      end
+
+      private
+
+      def channels(token)
+        token_decoded = JWT.decode token, nil, false
+        capability = token_decoded[0]['x-ably-capability']
+        channels_hash = JSON.parse(capability)
+
+        channels_hash.keys.join(',')
       end
     end
   end
--- lib/splitclient-rb/split_config.rb
+++ lib/splitclient-rb/split_config.rb
@@ -104,6 +104,7 @@ module SplitIoClient
 
       @sse_host_url = opts[:sse_host_url] || SplitConfig.default_sse_host_url
       @sse_block_until_ready = opts[:sse_block_until_ready] || SplitConfig.default_sse_block_until_ready
+      @auth_service_url = opts[:auth_service_url] || SplitConfig.default_auth_service_url
 
       startup_log
     end
@@ -261,12 +262,18 @@ module SplitIoClient
 
     attr_accessor :sse_block_until_ready
 
+    attr_accessor :auth_service_url
+
     def self.default_sse_host_url
       'https://realtime.ably.io/event-stream'
     end
 
     def self.default_sse_block_until_ready
       1
+    end
+
+    def self.default_auth_service_url
+      'https://auth.split-stage.io/api/auth'
     end
 
     #
