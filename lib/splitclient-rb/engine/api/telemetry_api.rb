d98f6692ddf75043984993c4a091182854500686
Mauro Sanz <mauro.sanz@split.io>
2021-05-26 18:33:18 -0300

    improvements

--- lib/splitclient-rb/engine/api/telemetry_api.rb
+++ lib/splitclient-rb/engine/api/telemetry_api.rb
@@ -10,34 +10,28 @@ module SplitIoClient
       end
 
       def record_init(config_init)
-        start = Time.now
-        response = post_api("#{@config.telemetry_service_url}/metrics/config", @api_key, config_init)
-
-        if response.success?
-          @config.split_logger.log_if_debug("Telemetry post success: record init.")
-          
-          bucket = BinarySearchLatencyTracker.get_bucket((Time.now - start) * 1000.0)
-          @telemetry_runtime_producer.record_sync_latency(Telemetry::Domain::Constants::TELEMETRY_SYNC, bucket)
-          @telemetry_runtime_producer.record_successful_sync(Telemetry::Domain::Constants::TELEMETRY_SYNC, (Time.now.to_f * 1000.0).to_i)
-        else
-          @telemetry_runtime_producer.record_sync_error(Telemetry::Domain::Constants::TELEMETRY_SYNC, response.status)
-          @config.logger.error("Unexpected status code while posting telemetry config: #{response.status}.")
-        end
+        post_telemetry("#{@config.telemetry_service_url}/metrics/config", config_init, 'init')
       end
 
       def record_stats(stats)
+        post_telemetry("#{@config.telemetry_service_url}/metrics/usage", stats, 'stats')
+      end
+
+      private
+
+      def post_telemetry(url, obj, method)
         start = Time.now
-        response = post_api("#{@config.telemetry_service_url}/metrics/usage", @api_key, stats)
+        response = post_api(url, @api_key, obj)
 
         if response.success?
-          @config.split_logger.log_if_debug("Telemetry post success: record stats.")
+          @config.split_logger.log_if_debug("Telemetry post succeeded: record #{method}.")
           
           bucket = BinarySearchLatencyTracker.get_bucket((Time.now - start) * 1000.0)
           @telemetry_runtime_producer.record_sync_latency(Telemetry::Domain::Constants::TELEMETRY_SYNC, bucket)
           @telemetry_runtime_producer.record_successful_sync(Telemetry::Domain::Constants::TELEMETRY_SYNC, (Time.now.to_f * 1000.0).to_i)
         else
           @telemetry_runtime_producer.record_sync_error(Telemetry::Domain::Constants::TELEMETRY_SYNC, response.status)
-          @config.logger.error("Unexpected status code while posting telemetry usage: #{response.status}.")
+          @config.logger.error("Unexpected status code while posting telemetry #{method}: #{response.status}.")
         end
       end
     end
