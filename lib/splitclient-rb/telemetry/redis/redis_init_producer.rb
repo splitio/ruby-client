675b08c2f0d3551bb4130569f4b4c7f907129058
Mauro Sanz <mauro.sanz@split.io>
2022-04-20 10:29:28 -0500

    added expire time

--- lib/splitclient-rb/telemetry/redis/redis_init_producer.rb
+++ lib/splitclient-rb/telemetry/redis/redis_init_producer.rb
@@ -3,6 +3,8 @@
 module SplitIoClient
   module Telemetry
     class RedisInitProducer < InitProducer
+      EXPIRE_SECONDS = 3600
+
       def initialize(config)
         @config = config
         @adapter = config.telemetry_adapter
@@ -14,7 +16,9 @@ module SplitIoClient
         data = { t: { oM: config_data.om, st: config_data.st, aF: config_data.af, rF: config_data.rf, t: config_data.t } }
         field = "#{@config.language}-#{@config.version}/#{@config.machine_name}/#{@config.machine_ip}"
 
-        @adapter.add_to_map(config_key, field, data.to_json)
+        result = @adapter.add_to_map(config_key, field, data.to_json)
+
+        @adapter.expire(config_key, EXPIRE_SECONDS) if result == 1
       rescue StandardError => e
         @config.log_found_exception(__method__.to_s, e)
       end
