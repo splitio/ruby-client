5a8491780fa34bc2b52f7363d77da64cc9f36dd6
Bilal Al-Shahwany <bilal@split.io>
2023-06-01 09:01:48 -0700

    used constants for compression level

--- lib/splitclient-rb/helpers/decryption_helper.rb
+++ lib/splitclient-rb/helpers/decryption_helper.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 module SplitIoClient
+  NO_COMPRESSION = 0
+  GZIP_COMPRESSION = 1
+  ZLIB_COMPRESSION = 2
+
   module Helpers
     class DecryptionHelper
       def self.get_encoded_definition(compression, data)
         case compression
-        when 0
+        when NO_COMPRESSION
           return Base64.decode64(data)
-        when 1
+        when GZIP_COMPRESSION
           gz = Zlib::GzipReader.new(StringIO.new(Base64.decode64(data)))
           return gz.read
-        when 2
+        when ZLIB_COMPRESSION
           return Zlib::Inflate.inflate(Base64.decode64(data))
         end
       end
