3eecb05c0db10433edb1180defbd50915caea1b3
Bilal Al-Shahwany <bilal@split.io>
2023-06-05 11:24:05 -0700

    polishing

--- lib/splitclient-rb/helpers/decryption_helper.rb
+++ lib/splitclient-rb/helpers/decryption_helper.rb
@@ -10,12 +10,14 @@ module SplitIoClient
       def self.get_encoded_definition(compression, data)
         case compression
         when NO_COMPRESSION
-          return Base64.decode64(data)
+          Base64.decode64(data)
         when GZIP_COMPRESSION
           gz = Zlib::GzipReader.new(StringIO.new(Base64.decode64(data)))
-          return gz.read
+          gz.read
         when ZLIB_COMPRESSION
-          return Zlib::Inflate.inflate(Base64.decode64(data))
+          Zlib::Inflate.inflate(Base64.decode64(data))
+        else
+          raise StandardError, 'Compression flag value is incorrect'
         end
       end
     end
