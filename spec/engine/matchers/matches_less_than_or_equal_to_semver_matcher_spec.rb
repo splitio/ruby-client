30232f8632c3d81ffe4db3b2bd93799aefe24517
Bilal Al <bilal.al-shahwany@split.io>
2024-03-29 11:01:04 -0700

    added less than or equal semver matcher

---
+++ spec/engine/matchers/matches_less_than_or_equal_to_semver_matcher_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SplitIoClient::LessThanOrEqualToSemverMatcher do
+  let(:raw) { {
+    'negate': false,
+    'matcherType': 'LESS_THAN_OR_EQUAL_TO_SEMVER',
+    'stringMatcherData': "2.1.8"
+  } }
+  let(:config) { SplitIoClient::SplitConfig.new }
+
+  it 'initilized params' do
+    matcher = described_class.new("version", raw[:stringMatcherData], config.split_logger, config.split_validator)
+    expect(matcher.attribute).to eq("version")
+    semver = matcher.instance_variable_get(:@semver)
+    expect(semver.instance_variable_get(:@old_version)).to eq("2.1.8")
+  end
+
+  it 'matches' do
+    matcher = described_class.new("version", raw[:stringMatcherData], config.split_logger, config.split_validator)
+    expect(matcher.match?(:attributes=>{"version": "2.1.8+rc"})).to eq(true)
+    expect(matcher.match?(:attributes=>{"version": "2.1.8"})).to eq(true)
+    expect(matcher.match?(:attributes=>{"version": "2.1.11"})).to eq(true)
+    expect(matcher.match?(:attributes=>{"version": "2.2.0"})).to eq(true)
+  end
+
+  it 'does not match' do
+    matcher = described_class.new("version", raw[:stringMatcherData], config.split_logger, config.split_validator)
+    expect(matcher.match?(:attributes=>{"version": "2.1.5"})).to eq(false)
+    expect(matcher.match?(:attributes=>{"version": "2.1.5-rc1"})).to eq(false)
+  end
+
+  it 'invalid attribute' do
+    matcher = described_class.new("version", raw[:stringMatcherData], config.split_logger, config.split_validator)
+    expect(matcher.match?(:attributes=>{"version": 2.1})).to eq(false)
+    expect(matcher.match?(:attributes=>{"version": nil})).to eq(false)
+  end
+
+end
