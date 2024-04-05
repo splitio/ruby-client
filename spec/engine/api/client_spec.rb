35743bdc7bf10ec078f06280810b2ff1ee5bb51f
Bilal Al <bilal.al-shahwany@split.io>
2024-04-05 12:46:48 -0700

    updated httpclient class

--- spec/engine/api/client_spec.rb
+++ spec/engine/api/client_spec.rb
@@ -2,8 +2,29 @@
 
 require 'spec_helper'
 
+class MyCustomDecorator
+  def get_header_overrides(request_context)
+      headers = request_context.headers
+      headers["UserCustomHeader"] = ["value"]
+      headers["AnotherCustomHeader"] = ["val1", "val2"]
+      headers
+  end
+end
+
+$headers = {}
+
+class MyRequestDecorator < SplitIoClient::Api::RequestDecorator
+  def initialize(custom_header_decorator)
+  end
+
+  def decorate_headers(headers)
+    $headers = headers
+    headers
+  end
+end
+
 describe SplitIoClient::Api::Client do
-  describe '#get_api' do
+  describe '#api' do
     it 'makes GET request without error' do
       url     = 'https://example.org?hello=world'
       api_key = 'abc-def-ghi'
@@ -11,7 +32,7 @@ describe SplitIoClient::Api::Client do
 
       stub_request(:get, url).to_return(status: 200)
 
-      expect { described_class.new(@default_config).get_api(url, api_key, params) }.not_to raise_error
+      expect { described_class.new(@default_config, SplitIoClient::Api::RequestDecorator.new(nil)).get_api(url, api_key, params) }.not_to raise_error
     end
 
     it 'makes POST request without error' do
@@ -21,7 +42,21 @@ describe SplitIoClient::Api::Client do
 
       stub_request(:post, url).to_return(status: 200)
 
-      expect { described_class.new(@default_config).post_api(url, api_key, data) }.not_to raise_error
+      expect { described_class.new(@default_config, SplitIoClient::Api::RequestDecorator.new(nil)).post_api(url, api_key, data) }.not_to raise_error
     end
+
+    it 'verify calling request decorator' do
+      url     = 'https://example.org?hello=world'
+      api_key = 'abc-def-ghi'
+      params  = { hello: :world }
+
+      stub_request(:get, url).to_return(status: 200)
+
+      client = described_class.new(@default_config, MyRequestDecorator.new(nil))
+      client.get_api(url, api_key, params)
+
+      expect($headers).to eq({"Accept-Encoding"=>"gzip,deflate"})
+    end
+
   end
 end
