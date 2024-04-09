61b8c9e2372c83b4b8fbaf9536423ea4d1046e34
Bilal Al <bilal.al-shahwany@split.io>
2024-04-08 19:58:23 -0700

    fixed condition

--- lib/splitclient-rb/engine/matchers/greater_than_or_equal_to_semver_matcher.rb
+++ lib/splitclient-rb/engine/matchers/greater_than_or_equal_to_semver_matcher.rb
@@ -10,20 +10,20 @@ module SplitIoClient
       super(logger)
       @validator = validator
       @attribute = attribute
-      @semver = SplitIoClient::Semver.new(string_value)
+      @semver = SplitIoClient::Semver.build(string_value, logger)
       @logger = logger
     end
 
     def match?(args)
       @logger.log_if_debug('[GreaterThanOrEqualsToSemverMatcher] evaluating value and attributes.')
-      return false unless @validator.valid_matcher_arguments(args)
+      return false unless @validator.valid_matcher_arguments(args) && args[:attributes][@attribute.to_sym].is_a?(String)
 
-      value_to_match = args[:attributes][@attribute.to_sym]
-      unless value_to_match.is_a?(String)
+      value_to_match = SplitIoClient::Semver.build(args[:attributes][@attribute.to_sym], @logger)
+      unless !value_to_match.nil? && !@semver.nil?
         @logger.log_if_debug('stringMatcherData is required for GREATER_THAN_OR_EQUAL_TO_SEMVER matcher type')
         return false
       end
-      matches = [0, 1].include?(SplitIoClient::Semver.new(value_to_match).compare(@semver))
+      matches = [0, 1].include?(value_to_match.compare(@semver))
       @logger.log_if_debug("[GreaterThanOrEqualsToSemverMatcher] #{value_to_match} matches -> #{matches}")
       matches
     end
