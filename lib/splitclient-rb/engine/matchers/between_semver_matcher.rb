8bf838b9c91242addf90b25c5a4319b592bc96a4
Bilal Al <bilal.al-shahwany@split.io>
2024-04-08 19:15:17 -0700

    used build for semver class

--- lib/splitclient-rb/engine/matchers/between_semver_matcher.rb
+++ lib/splitclient-rb/engine/matchers/between_semver_matcher.rb
@@ -10,22 +10,22 @@ module SplitIoClient
       super(logger)
       @validator = validator
       @attribute = attribute
-      @semver_start = SplitIoClient::Semver.new(start_value)
-      @semver_end = SplitIoClient::Semver.new(end_value)
+      @semver_start = SplitIoClient::Semver.build(start_value, logger)
+      @semver_end = SplitIoClient::Semver.build(end_value, logger)
       @logger = logger
     end
 
     def match?(args)
       @logger.log_if_debug('[BetweenSemverMatcher] evaluating value and attributes.')
-      return false unless @validator.valid_matcher_arguments(args)
+      return false unless @validator.valid_matcher_arguments(args) && args[:attributes][@attribute.to_sym].is_a?(String)
 
-      value_to_match = args[:attributes][@attribute.to_sym]
-      unless value_to_match.is_a?(String)
+      value_to_match = SplitIoClient::Semver.build(args[:attributes][@attribute.to_sym], @logger)
+      unless !value_to_match.nil? && !@semver_start.nil? && !@semver_end.nil?
         @logger.log_if_debug('betweenStringMatcherData is required for BETWEEN_SEMVER matcher type')
         return false
       end
-      matches = ([0, -1].include?(@semver_start.compare(SplitIoClient::Semver.new(value_to_match))) &&
-                 [0, 1].include?(@semver_end.compare(SplitIoClient::Semver.new(value_to_match))))
+      matches = ([0, -1].include?(@semver_start.compare(value_to_match)) &&
+                 [0, 1].include?(@semver_end.compare(value_to_match)))
       @logger.log_if_debug("[BetweenMatcher] #{value_to_match} matches -> #{matches}")
       matches
     end
