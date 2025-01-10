8606c0f86301ba6ed94be77e2b034ad12d5764a7
Bilal Al <bilal.al-shahwany@split.io>
2025-01-10 10:32:24 -0800

    added integration test for imp toggle

--- spec/integrations/in_memory_client_spec.rb
+++ spec/integrations/in_memory_client_spec.rb
@@ -1199,7 +1199,118 @@ describe SplitIoClient do
       result = client1.get_treatments_with_config_by_flag_sets('nico_test', ['set_2', 'set_3'])
       expect(result[:FACUNDO_TEST]).to eq({:config=>"{\"color\":\"green\"}", :treatment=>"on"})
     end
+  end
 
+  context 'impressions toggle' do
+    it 'optimized mode' do
+      splits_imp_toggle = File.read(File.join(SplitIoClient.root, 'spec/test_data/splits/imp-toggle.json'))
+      stub_request(:get, 'https://sdk.split.io/api/splitChanges?s=1.1&since=-1')
+          .to_return(status: 200, body: splits_imp_toggle)
+      factory_imp_toggle =
+        SplitIoClient::SplitFactory.new('test_api_key',
+          impressions_mode: :optimized,
+          features_refresh_rate: 9999,
+          telemetry_refresh_rate: 99999,
+          impressions_refresh_rate: 99999,
+          streaming_enabled: false)
+
+      client_imp_toggle = factory_imp_toggle.client
+      client_imp_toggle.block_until_ready
+
+      expect(client_imp_toggle.get_treatment('key1', 'with_track_disabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key2', 'with_track_enabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key3', 'without_track')).to eq('off')
+
+      impressions_repository = client_imp_toggle.instance_variable_get(:@impressions_repository)
+      imps = impressions_repository.batch
+      expect(imps.length()).to eq(2)
+      expect(imps[0][:i][:f]).to eq('with_track_enabled')
+      expect(imps[1][:i][:f]).to eq('without_track')
+
+      unique_keys_tracker = factory_imp_toggle.instance_variable_get(:@unique_keys_tracker)
+      unique_keys = unique_keys_tracker.instance_variable_get(:@cache)
+      expect(unique_keys.key?('with_track_disabled')).to eq(true)
+      expect(unique_keys.length).to eq(1)
+      impression_counter = factory_imp_toggle.instance_variable_get(:@impression_counter)
+      imp_count = impression_counter.pop_all
+      expect(imp_count.keys()[0].include? ('with_track_disabled')).to eq(true)
+      expect(imp_count.length).to eq(1)
+      client_imp_toggle.destroy
+      sleep 1
+    end
+    it 'debug mode' do
+      splits_imp_toggle = File.read(File.join(SplitIoClient.root, 'spec/test_data/splits/imp-toggle.json'))
+      stub_request(:get, 'https://sdk.split.io/api/splitChanges?s=1.1&since=-1')
+          .to_return(status: 200, body: splits_imp_toggle)
+      factory_imp_toggle =
+        SplitIoClient::SplitFactory.new('test_api_key',
+          impressions_mode: :debug,
+          features_refresh_rate: 9999,
+          telemetry_refresh_rate: 99999,
+          impressions_refresh_rate: 99999,
+          streaming_enabled: false)
+
+      client_imp_toggle = factory_imp_toggle.client
+      client_imp_toggle.block_until_ready
+
+      expect(client_imp_toggle.get_treatment('key1', 'with_track_disabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key2', 'with_track_enabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key3', 'without_track')).to eq('off')
+
+      impressions_repository = client_imp_toggle.instance_variable_get(:@impressions_repository)
+      imps = impressions_repository.batch
+      expect(imps.length()).to eq(2)
+      expect(imps[0][:i][:f]).to eq('with_track_enabled')
+      expect(imps[1][:i][:f]).to eq('without_track')
+
+      unique_keys_tracker = factory_imp_toggle.instance_variable_get(:@unique_keys_tracker)
+      unique_keys = unique_keys_tracker.instance_variable_get(:@cache)
+      expect(unique_keys.key?('with_track_disabled')).to eq(true)
+      expect(unique_keys.length).to eq(1)
+      impression_counter = factory_imp_toggle.instance_variable_get(:@impression_counter)
+      imp_count = impression_counter.pop_all
+      expect(imp_count.keys()[0].include? ('with_track_disabled')).to eq(true)
+      expect(imp_count.length).to eq(1)
+      client_imp_toggle.destroy
+      sleep 1
+    end
+    it 'debug mode' do
+      splits_imp_toggle = File.read(File.join(SplitIoClient.root, 'spec/test_data/splits/imp-toggle.json'))
+      stub_request(:get, 'https://sdk.split.io/api/splitChanges?s=1.1&since=-1')
+          .to_return(status: 200, body: splits_imp_toggle)
+      factory_imp_toggle =
+        SplitIoClient::SplitFactory.new('test_api_key',
+          impressions_mode: :none,
+          features_refresh_rate: 9999,
+          telemetry_refresh_rate: 99999,
+          impressions_refresh_rate: 99999,
+          streaming_enabled: false)
+
+      client_imp_toggle = factory_imp_toggle.client
+      client_imp_toggle.block_until_ready
+      expect(client_imp_toggle.get_treatment('key1', 'with_track_disabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key2', 'with_track_enabled')).to eq('off')
+      expect(client_imp_toggle.get_treatment('key3', 'without_track')).to eq('off')
+
+      impressions_repository = client_imp_toggle.instance_variable_get(:@impressions_repository)
+      imps = impressions_repository.batch
+      expect(imps.length()).to eq(0)
+
+      unique_keys_tracker = factory_imp_toggle.instance_variable_get(:@unique_keys_tracker)
+      unique_keys = unique_keys_tracker.instance_variable_get(:@cache)
+      expect(unique_keys.key?('with_track_disabled')).to eq(true)
+      expect(unique_keys.key?('with_track_enabled')).to eq(true)
+      expect(unique_keys.key?('without_track')).to eq(true)
+      expect(unique_keys.length).to eq(3)
+      impression_counter = factory_imp_toggle.instance_variable_get(:@impression_counter)
+      imp_count = impression_counter.pop_all
+      expect(imp_count.keys()[0].include? ('with_track_disabled')).to eq(true)
+      expect(imp_count.keys()[1].include? ('with_track_enabled')).to eq(true)
+      expect(imp_count.keys()[2].include? ('without_track')).to eq(true)
+      expect(imp_count.length).to eq(3)
+      client_imp_toggle.destroy
+      sleep 1
+    end
   end
 end
 
