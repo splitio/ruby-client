178bcf4592690bb1b87cbfa5fe3b25b8a78a32db
Bilal Al-Shahwany <bilal.al-shahwany@harness.io>
2026-02-13 14:53:59 -0800

    added events task and polishing.

--- lib/splitclient-rb/engine/events/events_manager_config.rb
+++ lib/splitclient-rb/engine/events/events_manager_config.rb
@@ -1,87 +1,96 @@
 # frozen_string_literal: true
 
-module SplitIoClient::Engine::Events
-    class EventsManagerConfig
+module SplitIoClient
+  module Engine
+    module Events
+      class EventsManagerConfig
         attr_accessor :require_all, :prerequisites, :require_any, :suppressed_by, :execution_limits, :evaluation_order
 
         def initialize
-            @require_all = get_require_all
-            @prerequisites = get_prerequisites
-            @require_any = get_require_any
-            @suppressed_by = get_suppressed_by
-            @execution_limits = get_execution_limits
-            @evaluation_order = get_sorted_events
+          @require_all = construct_require_all
+          @prerequisites = construct_prerequisites
+          @require_any = construct_require_any
+          @suppressed_by = construct_suppressed_by
+          @execution_limits = construct_execution_limits
+          @evaluation_order = construct_sorted_events
         end
-                        
+
         private
 
-        def get_require_all
-            return  {
-                        SplitIoClient::Engine::Models::SdkEvent::SDK_READY => Set.new([SplitIoClient::Engine::Models::SdkInternalEvent::SDK_READY])
-                    }
+        def construct_require_all
+          {
+            SplitIoClient::Engine::Models::SdkEvent::SDK_READY => Set.new([SplitIoClient::Engine::Models::SdkInternalEvent::SDK_READY])
+          }
         end
 
-        def get_prerequisites
-            return  {
-                        SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => Set.new([SplitIoClient::Engine::Models::SdkEvent::SDK_READY])
-                    }
+        def construct_prerequisites
+          {
+            SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => Set.new([SplitIoClient::Engine::Models::SdkEvent::SDK_READY])
+          }
         end
 
-        def get_require_any
-            return  {
-                        SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => Set.new([SplitIoClient::Engine::Models::SdkInternalEvent::FLAG_KILLED_NOTIFICATION, SplitIoClient::Engine::Models::SdkInternalEvent::FLAGS_UPDATED, 
-                                                SplitIoClient::Engine::Models::SdkInternalEvent::RB_SEGMENTS_UPDATED, SplitIoClient::Engine::Models::SdkInternalEvent::SEGMENTS_UPDATED])
-                    }
+        def construct_require_any
+          {
+            SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => Set.new(
+              [
+                SplitIoClient::Engine::Models::SdkInternalEvent::FLAG_KILLED_NOTIFICATION,
+                SplitIoClient::Engine::Models::SdkInternalEvent::FLAGS_UPDATED,
+                SplitIoClient::Engine::Models::SdkInternalEvent::RB_SEGMENTS_UPDATED,
+                SplitIoClient::Engine::Models::SdkInternalEvent::SEGMENTS_UPDATED
+              ]
+            )
+          }
         end
 
-        def get_suppressed_by
-            return  {}
+        def construct_suppressed_by
+          {}
         end
 
-        def get_execution_limits
-            return  {
-                        SplitIoClient::Engine::Models::SdkEvent::SDK_READY => 1,
-                        SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => -1
-                    }
+        def construct_execution_limits
+          {
+            SplitIoClient::Engine::Models::SdkEvent::SDK_READY => 1,
+            SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE => -1
+          }
         end
 
-        def get_sorted_events
-            sorted_events = []
-            for sdk_event in [SplitIoClient::Engine::Models::SdkEvent::SDK_READY, SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE]
-                sorted_events = dfs_recursive(sdk_event, sorted_events)
-            end
+        def construct_sorted_events
+          sorted_events = []
+          [SplitIoClient::Engine::Models::SdkEvent::SDK_READY, SplitIoClient::Engine::Models::SdkEvent::SDK_UPDATE].each do |sdk_event|
+            sorted_events = dfs_recursive(sdk_event, sorted_events)
+          end
 
-            return sorted_events
+          sorted_events
         end
 
         def dfs_recursive(sdk_event, added)
-            return added if added.include?(sdk_event)
+          return added if added.include?(sdk_event)
 
-            get_dependencies(sdk_event).each do |dependent_event|
-                added = dfs_recursive(dependent_event, added)
-            end
+          get_dependencies(sdk_event).each do |dependent_event|
+            added = dfs_recursive(dependent_event, added)
+          end
 
-            added.push(sdk_event)
-            return added
+          added.push(sdk_event)
+
+          added
         end
 
         def get_dependencies(sdk_event)
-            dependencies = Set.new
-            @prerequisites.each do |prerequisites_event_name, prerequisites_event_value|
-                if prerequisites_event_name == sdk_event
-                    for prereq_event in prerequisites_event_value
-                        dependencies.add(prereq_event)
-                    end
-                end
-            end
+          dependencies = Set.new
+          @prerequisites.each do |prerequisites_event_name, prerequisites_event_value|
+            skip unless prerequisites_event_name == sdk_event
 
-            @suppressed_by.each do |suppressed_event_name, suppressed_event_value|
-                if suppressed_event_value.include?(sdk_event)
-                    dependencies.add(suppressed_event_name)
-                end
+            prerequisites_event_value.each do |prereq_event|
+              dependencies.add(prereq_event)
             end
+          end
 
-            return dependencies
+          @suppressed_by.each do |suppressed_event_name, suppressed_event_value|
+            dependencies.add(suppressed_event_name) if suppressed_event_value.include?(sdk_event)
+          end
+
+          dependencies
         end
+      end
     end
+  end
 end
